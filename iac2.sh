4a231a981ec53ce7dd2e968288cfb938cba8fa31
Lucas Soares Manesco <100358020+lucasmanesco@users.noreply.github.com>
2023-04-10 08:16:54 -0300

    Update iac2.sh

--- iac2.sh
+++ iac2.sh
@@ -12,10 +12,9 @@ echo "Instalando pacotes..."
 apt install apache2 -y
 apt install unzip -y
 
-"Baixando e copiando os arquivos da aplicação..."
+echo "Baixando e copiando os arquivos da aplicação..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
 cd linux-site-dio-main
 cp -R * /var/www/html
-
